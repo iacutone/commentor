4897619a35c2b0666d3cb59b8cddc83e7504180f
Eric Iacutone <eric.iacutone@gmail.com>
2016-05-13 12:54:01 -0300

    finish refactoring

--- web/controllers/api_controller.ex
+++ web/controllers/api_controller.ex
@@ -8,7 +8,12 @@ defmodule Commentor.ApiController do
   def trello(conn, _params) do
     user = Repo.get_by(User, uid: Integer.to_string(_params["sender"]["id"]))
 
-    card_url = _params["pull_request"]["body"]
+    cond do
+      _params["pull_request"] ->
+        card_url = _params["pull_request"]["body"]
+      _params["issue"] ->
+        card_url = _params["issue"]["body"]
+    end
 
     if card_url do
       url_contains_trello = Regex.match?(~r[trello], card_url)
@@ -19,19 +24,25 @@ defmodule Commentor.ApiController do
       card_id = Enum.at(card_list, 4)
     end
 
-    if user && card_url && card_id do
+    if !is_nil(user) && !is_nil(card_id) do
       key = System.get_env("TRELLO_KEY")
       token = System.get_env("TRELLO_TOKEN")
 
-      pr_url = _params["pull_request"]["url"]
+      cond do
+        _params["pull_request"] ->
+          pr_url = _params["pull_request"]["url"]
+        _params["issue"] ->
+          pr_url = _params["issue"]["html_url"]
+      end
+
       comment_url = String.replace(pr_url, "https://", "")
-      comment = comment_url <> " " <> " sent from Commentor"
+      comment = comment_url <> String.replace(" sent from Commentor", " ", "%20")
 
       HTTPoison.post "https://trello.com/1/cards/#{card_id}/actions/comments?key=#{key}&token=#{token}&text=#{comment}", ""
     end
 
     conn
       |> put_status(:ok)
-      # |> json( %{ resp_type: :success} )
+      |> json( %{ resp_type: :success} )
   end
 end
